4d5f79d3f3b12eafa617e56154d7f1077e517547
bautilugano22 <bautilugano22@gmail.com>
2026-08-26 16:16:25 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import '../styles/globals.css';
+import Script from 'next/script';
+
+const ADSENSE_CLIENT = process.env.NEXT_PUBLIC_ADSENSE_CLIENT;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {ADSENSE_CLIENT && (
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_CLIENT}`}
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
